f5c6d0525b0cd3632159a7cc35e63f4f3ff750a8
Michal Siminski <michal.siminski3@gmail.com>
2020-12-27 09:47:39 +0100

    add snow using GSAP

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
     const pin = document.querySelector('.pin');
     let count = 0;
+
+    //////
+    // pin
     const ball = () => {
         count += 2;
         pin.style.marginTop = `${count}px`;
@@ -9,3 +12,15 @@
         }
     }
     ball();
+    //////
+    // snow
+
+    TweenMax.fromTo('.snow', 15, {
+        y: 0,
+        x: 50
+    }, {
+        y: 100 + 'vh',
+        x: 250,
+        repeat: -1,
+        ease: "power3.out"
+    })
