5eac402b2b0fc664b7f7eb191f2db39b26d88319
Michal Siminski <michal.siminski3@gmail.com>
2020-12-27 13:04:56 +0100

    add santa sledge + GSAP

--- main.js
+++ main.js
@@ -15,7 +15,7 @@
     //////
     // snow
 
-    TweenMax.fromTo('.snow', 15, {
+    TweenMax.fromTo('.snow', 10, {
         y: 0,
         x: 50
     }, {
@@ -24,3 +24,28 @@
         repeat: -1,
         ease: "power3.out"
     })
+    //////
+    // santa
+    const santaSledge = document.querySelector('.santaSledge');
+
+    const santaAnim = new TimelineMax({
+        repeat: -1
+    });
+    santaAnim.set(santaSledge, {
+        x: -500 + 'px',
+        y: 80 + 'vh',
+        rotation: 10,
+        repeat: -1,
+    });
+    santaAnim.to(santaSledge, 4, {
+        x: 35 + 'vw',
+        y: 75 + 'vh',
+        rotation: 5,
+        ease: "none"
+    });
+    santaAnim.to(santaSledge, 4, {
+        x: 100 + 'vw',
+        rotation: -5,
+        y: 50 + 'vh',
+        ease: "none"
+    });
