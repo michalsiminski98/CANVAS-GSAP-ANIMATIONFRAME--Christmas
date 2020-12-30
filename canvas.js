2fe045fd23d3f9389cde7eb7db81b2394a0239f9
Michal Siminski <michal.siminski3@gmail.com>
2020-12-30 07:39:05 +0100

    add canvas snow

--- canvas.js
+++ canvas.js
@@ -1,17 +1,89 @@
-// canvas parameter
-const canvas = document.querySelector('#canvas');
-const ctx = canvas.getContext('2d');
+class Sky {
+    constructor(canvas) {
+        this.canvas = canvas;
+        this.ctx = canvas.getContext('2d');
+        this.width = window.innerWidth;
+        this.height = window.innerHeight;
+    }
 
-// describe snow
-ctx.fillStyle = '#f00'
-ctx.fillRect(0, 0, 100, 50);
+    initCanvas() {
+        this.canvas.width = this.width;
+        this.canvas.height = this.height;
 
-// start drawing line
-ctx.beginPath();
-ctx.lineWidth = 3;
-ctx.strokeStyle = '#0f0'
-ctx.moveTo(50, 50);
-ctx.lineTo(100, 100);
-ctx.lineTo(200, 150);
-// ctx.closePath(); //closed line
-ctx.stroke();
+        this.ctx.fillStyle = 'rgb(44, 73, 153)';
+        this.ctx.fillRect(0, 0, this.width, this.height)
+    }
+
+    generateSnows(count) {
+        let snows = [];
+
+        for (let i = 0; i < count; i++) {
+            const radius = Math.random() * 3 + 2;
+
+            snows.push({
+                x: Math.random() * this.width,
+                y: Math.random() * this.height,
+                radius: radius,
+                color: '#fff',
+                speed: Math.random() + 0.25,
+            })
+        }
+
+        this.snows = snows;
+    }
+
+    drawSnows() {
+        this.snows.forEach(snow => {
+            this.drawSnow(snow);
+        })
+    }
+
+    updateSnows() {
+        this.snows.forEach(snow => {
+            snow.y += snow.speed;
+            snow.x += snow.speed;
+            if (snow.y > this.height + 2 * snow.radius) {
+                snow.y = -2 * snow.radius
+            }
+            if (snow.x > this.width + 2 * snow.radius) {
+                snow.x = -2 * snow.radius
+            }
+        })
+    }
+
+    clearCanvas() {
+        this.ctx.fillStyle = 'rgb(44, 73, 153)';
+        this.ctx.fillRect(0, 0, this.width, this.height)
+    }
+
+    drawSnow(snow) {
+        this.ctx.save();
+
+        this.ctx.fillStyle = snow.color;
+
+        this.ctx.beginPath();
+
+        this.ctx.translate(snow.x, snow.y);
+        this.ctx.moveTo(0, 0 - snow.radius);
+        this.ctx.arc(100, 35, 1, 0, 2 * Math.PI);
+
+        this.ctx.fill();
+        this.ctx.restore();
+    }
+
+    draw() {
+        this.clearCanvas();
+        this.drawSnows();
+        this.updateSnows();
+        window.requestAnimationFrame(() => this.draw());
+    }
+
+    run() {
+        this.initCanvas();
+        this.generateSnows(500);
+        this.draw();
+    }
+}
+
+const sky = new Sky(document.querySelector('#canvas'));
+sky.run();
